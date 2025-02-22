a6766f00ff661b08996326302717b7dd21b69ac4
llalma <lhulsmanbenson@gmail.com>
2025-02-22 22:12:32 +1000

    remove unneeded code

--- testing/save_page.js
+++ testing/save_page.js
@@ -5,85 +5,10 @@ const crypto = require("crypto");
 
 const test_folder = "tests";
 
-// Function to get full XPath
-const getFullXPath = (element) => {
-  let xpath = "";
-  while (element && element.nodeType === Node.ELEMENT_NODE) {
-    let count = 0;
-    let sibling = element;
-    while (sibling) {
-      if (
-        sibling.nodeType === Node.ELEMENT_NODE &&
-        sibling.nodeName === element.nodeName
-      ) {
-        count++;
-      }
-      sibling = sibling.previousSibling;
-    }
-    let tagName = element.nodeName.toLowerCase();
-    let index = count > 1 ? `[${count}]` : "";
-    xpath = `/${tagName}${index}${xpath}`;
-    element = element.parentNode;
-  }
-  return xpath;
-};
-
 async function save_page(url, xpath, expected) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2" });
-
-  // Function to get full XPath
-  const getFullXPath = (element) => {
-    let xpath = "";
-    while (element && element.nodeType === Node.ELEMENT_NODE) {
-      let count = 0;
-      let sibling = element;
-      while (sibling) {
-        if (
-          sibling.nodeType === Node.ELEMENT_NODE &&
-          sibling.nodeName === element.nodeName
-        ) {
-          count++;
-        }
-        sibling = sibling.previousSibling;
-      }
-      let tagName = element.nodeName.toLowerCase();
-      let index = count > 1 ? `[${count}]` : "";
-      xpath = `/${tagName}${index}${xpath}`;
-      element = element.parentNode;
-    }
-    return xpath;
-  };
-
-  // Inject script to modify the HTML
-  await page.evaluate(() => {
-    function getFullXPath(element) {
-      let xpath = "";
-      while (element && element.nodeType === Node.ELEMENT_NODE) {
-        let count = 0;
-        let sibling = element;
-        while (sibling) {
-          if (
-            sibling.nodeType === Node.ELEMENT_NODE &&
-            sibling.nodeName === element.nodeName
-          ) {
-            count++;
-          }
-          sibling = sibling.previousSibling;
-        }
-        let tagName = element.nodeName.toLowerCase();
-        let index = count > 1 ? `[${count}]` : "";
-        xpath = `/${tagName}${index}${xpath}`;
-        element = element.parentNode;
-      }
-      return xpath;
-    }
-
-    document.querySelectorAll("*").forEach((el) => {
-      el.setAttribute("data-xpath", getFullXPath(el));
-    });
-  });
 
   // Create dir if it dosent exist
   const folder_name = `${test_folder}/${crypto.randomUUID()}`;
